5e658c0e941d59ec71621d25834ff206496bbc88
devak23 <developer.ak23@gmail.com>
2017-08-01 23:11:25 +0530

    Configured a component

--- react-basics/src/app/index.js
+++ react-basics/src/app/index.js
@@ -1,3 +1,15 @@
-let message = "this is the entry point";
-console.info('It works!!');
-console.debug(message);
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        console.debug('invoking the render method');
+        return (
+          <div className="container">
+              <h1 className="jumbotron">Todo Lists</h1>
+          </div>
+        );
+    }
+}
+
+render(<App/>, document.getElementById("app"));
