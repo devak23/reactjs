c2ce43a60afb9b2ff22eb219111ea01b22aba547
devak23 <developer.ak23@gmail.com>
2017-08-02 13:05:50 +0530

    Enabled communication between components

--- react-basics/src/app/components/Home.js
+++ react-basics/src/app/components/Home.js
@@ -6,8 +6,14 @@ export class Home extends React.Component {
             <div className="row">
                 <div className="col-xs-4 col-offset-1">
                     <div className="panel panel-default">
-                        <p className="panel-heading">Posts</p>
-                        <p className="panel-body">In a new Component!</p>
+                        <p className="panel-heading">
+                            <span className="pull-left">{this.props.user.name} ({this.props.user.age})</span>
+                            <span className="pull-right">{this.props.user.department}</span>
+                            <span className="clearfix"/>
+                        </p>
+                        <p className="panel-body">
+                            <strong>TEAM: </strong> {this.props.user.team.join(", ")}
+                        </p>
                     </div>
                 </div>
             </div>
--- react-basics/src/app/index.js
+++ react-basics/src/app/index.js
@@ -5,21 +5,35 @@ import { Home } from './components/Home';
 
 class App extends React.Component {
     render() {
-        console.log('invoking the render method');
+        var abhay = {
+            name: "Abhay K",
+            age: 38,
+            department: 'Software',
+            team: ["Datta", "Sandeep", "Vidyadhar", "Pratima", "Ranjit", "Madhuri"]
+        };
+
+        var vinod = {
+            name: "Vinod K",
+            age: 38,
+            department: 'Streaming',
+            team: ["KD", "Dinesh", "Asif"]
+        };
+
         return (
           <div className="container">
               <div className="row">
                   <div className="col-xs-10 col-offset-1">
-                      <Header></Header>
+                      <Header/>
                   </div>
               </div>
               <div className="row">
                   <div className="col-xs-10 col-offset-1">
-                      <Home></Home>
+                      <Home user={abhay}/>
+                      <Home user={vinod}/>
                   </div>
               </div>
           </div>
-        );
+        )
     }
 }
 
