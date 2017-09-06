a4b33d55e196d32611a5194c7a9e46a643795d06
devak23 <developer.ak23@gmail.com>
2017-09-06 22:30:50 +0530

    added a Form to the page

--- react-chat/src/MessagePane/Form/index.js
+++ react-chat/src/MessagePane/Form/index.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Props from "prop-types";
 
-const Form = () => (
-  <div>Form</div>
-);
+// this is a stateful component. Hence needs to extend the component.
+// else a stateless component (like MessagePane) would suffice.
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      message: ''
+    };
+  }
+
+
+  onSubmit() {
+    const {name, message} = this.state;
+    console.log(name, message);
+    this.props.onSend(name, message);
+    this.setState({name: '', message: ''});
+
+  }
+
+  updateName(event) {
+    this.setState({
+      name: event.target.value.trim()
+    });
+  }
+
+  updateMessage(event) {
+    this.setState({
+      message: event.target.value.trim()
+    });
+  }
+
+  render() {
+    return (
+      <div className="MessagePane-Form">
+        <div className="MessagePane-Form-container">
+          <p>
+            <input
+              // ref={(name) => {this.name_input = name}}
+              className="name"
+              type="text"
+              placeholder="Your name"
+              value={this.state.name}
+              onChange={this.updateName.bind(this)}
+            />
+          </p>
+          <p>
+            <textarea
+              // ref={(message) => {this.message_input = message}}
+              className="message"
+              placeholder="Your message"
+              value={this.state.message}
+              onChange={this.updateMessage.bind(this)}
+            />
+          </p>
+          <p>
+            <button className="send" onClick={this.onSubmit.bind(this)}>Send</button>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+Form.propTypes = {
+  onSend: Props.func.isRequired
+};
+
+Form.defaultProps = {
+  onSend: () => {}
+};
 
 export default Form;
