40295c6c22e257e4870a7ce4b5d7d285f3ca0fc6
devak23 <developer.ak23@gmail.com>
2017-09-08 19:14:54 +0530

    Minor refactoring

--- bookworm/bookworm-react/src/index.js
+++ bookworm/bookworm-react/src/index.js
@@ -1,3 +1,4 @@
+import "semantic-ui-css/semantic.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,7 +8,6 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
 import App from './App';
-import "semantic-ui-css/semantic.min.css";
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from "./rootReducer";
 
