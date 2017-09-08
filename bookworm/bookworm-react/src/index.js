763bc92244170b6cb0fad6bf4fe5f0cc293acb6b
devak23 <developer.ak23@gmail.com>
2017-09-08 16:02:35 +0530

    Redux setup

--- bookworm/bookworm-react/src/index.js
+++ bookworm/bookworm-react/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import App from './App';
 import "semantic-ui-css/semantic.min.css";
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from "./rootReducer";
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <BrowserRouter>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </BrowserRouter>
   , document.getElementById('root'));
 registerServiceWorker();
