dcabc9425c63e7a74551b38c6f9e90a8f2d432ad
devak23 <developer.ak23@gmail.com>
2017-09-12 15:58:12 +0530

    Initial commit

---
+++ bookworm/bookworm-api/src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.post('/api/auth', (req, res) => {
+  res.status(400).json({ errors: { global: "invalid credentials" }})
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+
+app.listen(9000, () => console.log('Server is running on localhost:9000'));
