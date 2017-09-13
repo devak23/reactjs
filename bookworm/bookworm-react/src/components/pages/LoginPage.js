76cbf63cf6368e778bfadd6b2b26d21f51f2cbda
devak23 <developer.ak23@gmail.com>
2017-09-13 06:07:40 +0530

    added comments

--- bookworm/bookworm-react/src/components/pages/LoginPage.js
+++ bookworm/bookworm-react/src/components/pages/LoginPage.js
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 import LoginForm from "../forms/LoginForm";
 import { login } from '../../actions/auth';
 
+// The login page contains the login form. It will pass the submit function to the
+// login form. The login form gathers data and invokes the submit function on this 
+// class. The login form doesn't know anything about what will happen to the data
+// it passes to the LoginPage.
 class LoginPage extends React.Component {
 
   submit = data => this.props.login(data).then(() => this.props.history.push('/'));
