9700c6f3ee0b188d389bb8b07c16905a5a3741eb
devak23 <developer.ak23@gmail.com>
2017-09-18 14:47:51 +0530

    persisting the signup data into localstorage

--- bookworm/bookworm-react/src/actions/users.js
+++ bookworm/bookworm-react/src/actions/users.js
@@ -2,6 +2,12 @@ import api from '../api';
 import { userLoggedIn } from './auth';
 
 const signup = data => dispatch =>
-  api.user.signup(data).then(user => dispatch(userLoggedIn(user)));
+  api.user.signup(data).then(user => {
+    localStorage.bookwormJWT = user.token;
+    localStorage.firstname = user.firstname;
+    localStorage.lastname = user.lastname;
+
+    dispatch(userLoggedIn(user))
+  });
 
 export default signup;
