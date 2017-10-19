7c161723d9cde7f0f9dcd33ea8a1fc00e9bd00e4
devak23 <developer.ak23@gmail.com>
2017-10-19 13:17:10 +0530

    removed hardcoding

--- dsw/src/components/TagCould.js
+++ dsw/src/components/TagCould.js
@@ -1,103 +1,97 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
 
+const refData = [
+  {
+    text: "Predictive Maintenance",
+    color: "#0063ad",
+    fontSize: "20px"
+  },
+  {
+    text: "Operational",
+    color: "#444",
+    fontSize: "14px"
+  },
+  {
+    text: "Deep Learning",
+    color: "#0063ad",
+    fontSize: "26px"
+  },
+  {
+    text: "Diagnostics Health & Safety",
+    color: "#0073c9",
+    fontSize: "24px"
+  },
+  {
+    text: "Environment Maintence & Reliability",
+    color: "#444",
+    fontSize: "18px"
+  },
+  {
+    text: "Sustainability",
+    color: "#214272",
+    fontSize: "22px"
+  },
+  {
+    text: "Projects Clustering",
+    color: "#0063ad",
+    fontSize: "12px"
+  },
+  {
+    text: "Regression Classification",
+    color: "#5b90ce",
+    fontSize: "24px"
+  },
+  {
+    text: "Predictive",
+    color: "#111",
+    fontSize: "20px"
+  },
+  {
+    text: "Time series",
+    color: "#5b90ce",
+    fontSize: "18px"
+  },
+  {
+    text: "Exploratory Hypothesis Analysis",
+    color: "#214272",
+    fontSize: "20px"
+  },
+  {
+    text: "Prescriptive",
+    color: "#666",
+    fontSize: "18px"
+  },
+  {
+    text: "Bayesian Network",
+    color: "#0063ad",
+    fontSize: "14px"
+  },
+  {
+    text: "Artificial Intelligence",
+    color: "#0073c9",
+    fontSize: "24px"
+  },
+  {
+    text: "Supply Chain",
+    color: "#777",
+    fontSize: "16px"
+  }
+];
+
 const TagCloud = () => (
   <Segment>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#0063ad",
-        fontSize: "20px"
-      }}
-    >
-      Predictive Maintenance
-    </span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#577094",
-        fontSize: "14px"
-      }}
-    >
-      Operational
-    </span>
-    <span>Deep Learning</span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#0063ad",
-        fontSize: "26px"
-      }}
-    >
-      Diagnostics Health & Safety
-    </span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#0073c9",
-        fontSize: "24px"
-      }}
-    >
-      Environment Maintence & Reliability
-    </span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#0063ad",
-        fontSize: "20px"
-      }}
-    >
-      Sustainability
-    </span>
-    <span>Projects Clustering</span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "##214272",
-        fontSize: "22px"
-      }}
-    >
-      Regression Classification
-    </span>
-    <span>Predictive</span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#0073c9",
-        fontSize: "20px"
-      }}
-    >
-      Time Series
-    </span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#5b90ce",
-        fontSize: "18px"
-      }}
-    >
-      Exploratory Hypothesis Analysis
-    </span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "##214272",
-        fontSize: "16px"
-      }}
-    >
-      Prescriptive
-    </span>
-    <span>Bayesian Network</span>
-    <span
-      style={{
-        margin: "2px 4px",
-        color: "#0063ad",
-        fontSize: "24px"
-      }}
-    >
-      Artificial Intelligence
-    </span>
-    <span style={{ fontSize: "18px" }}>Supply Chain</span>
+    {refData.map(item => (
+      <span
+        style={{
+          margin: "2px 4px",
+          color: item.color,
+          fontSize: item.fontSize
+        }}
+      >
+        {item.text}
+      </span>
+    ))}
   </Segment>
 );
 
