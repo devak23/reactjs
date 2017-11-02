d3abe4ea5115e071233a1da8e80173be4efeb200
devak23 <developer.ak23@gmail.com>
2017-11-02 05:43:45 +0530

    Extracted a message component

--- bgshop/src/components/GamesList.js
+++ bgshop/src/components/GamesList.js
@@ -1,19 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import GameCard from "./GameCard";
-
-// Emmet code: .ui.icon.message>i.icon.info+.content>.header+p
+import Message from "./Message";
 
 const GamesList = ({ games }) => (
   <div className="ui four cards">
     {games.length === 0 ? (
-      <div className="ui icon message">
-        <i className="icon info" />
-        <div className="content">
-          <div className="header">There are no games in your store</div>
-          <p>You should add some. Dont you think?</p>
-        </div>
-      </div>
+      <Message
+        content="You should add some. Dont you think?"
+        type="error"
+        header="There are no games in your store"
+      />
     ) : (
       games.map(game => <GameCard key={game.id} game={game} />)
     )}
---
+++ bgshop/src/components/Message.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Icon } from "semantic-ui-react";
+
+// Emmet code: .ui.icon.message>i.icon.info+.content>.header+p
+
+const Message = ({ header, content, type }) => {
+  const getIcon = type =>
+    type === "error"
+      ? "warning sign"
+      : type === "success" ? "checkmark" : "info"; // info to be defaulted
+
+  const getColor = type =>
+    type === "error" ? "red" : type === "success" ? "green" : ""; //info wont have any color
+
+  return (
+    <div>
+      <div className={`ui icon message ${getColor(type)}`}>
+        <Icon name={getIcon(type)} />
+        <div className="content">
+          <div className="header">{header}</div>
+          <p>{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Message.propTypes = {
+  header: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
+};
+
+Message.defaultProps = {
+  header: "<<A message needs a header>>",
+  content: "<<Content for the message component is expected>>",
+  type: "info"
+};
+
+export default Message;
