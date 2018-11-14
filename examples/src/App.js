c7017826616b6953af703f985d22d3c17d629494
devak23 <developer.ak23@gmail.com>
2018-11-14 09:10:36 +0530

    Added custom CSS and fixed the counter issue

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './App.css'
 import 'semantic-ui-css/semantic.min.css';
 import Banner from './components/banner';
 import Counter from './components/counter'
 
 const App = () => (
     <div className="container">
-        <Banner message={'Welcome to the lovely world of ReactJS'}
+        <Banner message={'There are two important days in your life. The day when you were born and the day when you know why.'}
             color={'orange'}
         />
+        <hr className="stylish" />
         <Counter count={150} />
+        <hr className="stylish" />
     </div>
 );
 
--- examples/src/components/counter.js
+++ examples/src/components/counter.js
@@ -3,28 +3,30 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: this.props.count
+        currentCount: this.props.count,
+        initialCount: this.props.count
     };
 
     increment = () => {
-        this.setState({ count: this.state.count + 1 });
+        this.setState({ currentCount: this.state.currentCount + 1 });
     }
 
     setValue = (e) => {
-        this.setState({ count: parseInt(e.target.value) });
+        this.setState({ currentCount: parseInt(e.target.value), initialCount: parseInt(e.target.value) });
     }
 
     render() {
-        let { count } = this.state;
-        let labelCount = this.props.count;
+        let { currentCount, initialCount } = this.state;
         return (
             <div className="">
                 <label htmlFor="initialCount">Initial Count: </label>
                 <div className="ui input">
-                    <input id="initialCount" name="initialCount" type="text" value={labelCount} onChange={this.setValue} />
+                    <input id="initialCount" name="initialCount" type="text" value={initialCount} onChange={this.setValue} />
                 </div>
-                <button className="ui button" onClick={this.increment}>Increment</button> <br />
-                <label>Value now is: {count}</label>
+                <div className="ui input">
+                    <button className="ui button" onClick={this.increment}>Increment</button>
+                </div>
+                <label>Current Count: {currentCount}</label>
             </div>
         )
     }
