f5ad89c6bb24fa0d8a74d2e34307e115d752204c
devak23 <developer.ak23@gmail.com>
2018-11-26 09:52:41 +0530

    Card details can now be toggled

--- kanban/src/components/Card.js
+++ kanban/src/components/Card.js
@@ -2,14 +2,31 @@ import React from "react";
 import CheckList from "./CheckList";
 
 class Card extends React.Component {
+  state = {
+    showDetails: false
+  };
   render() {
-    return (
-      <div className="card">
-        <div className="card__title">{this.props.title}</div>
+    let cardDetails;
+
+    if (this.state.showDetails) {
+      cardDetails = (
         <div className="card__details">
           {this.props.description}
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
+      );
+    }
+    return (
+      <div className="card">
+        <div
+          className="card__title"
+          onClick={() =>
+            this.setState({ showDetails: !this.state.showDetails })
+          }
+        >
+          {this.props.title}
+        </div>
+        {cardDetails}
       </div>
     );
   }
