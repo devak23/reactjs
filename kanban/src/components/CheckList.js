546ed8e506e5cbcc659b85327166eae11aa3b3e4
devak23 <developer.ak23@gmail.com>
2018-11-27 10:09:45 +0530

    added a text field to add tasks

--- kanban/src/components/CheckList.js
+++ kanban/src/components/CheckList.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class CheckList extends Component {
   render() {
     let tasks = this.props.tasks.map(t => (
-      <li className="checklist__task">
+      <li className="checklist__task" key={t.id}>
         <input type="checkbox" name="" id="" defaultChecked={t.done} /> {t.name}
         <a href="#" className="checklist__task--remove fa fa-remove" />
       </li>
@@ -11,6 +11,11 @@ class CheckList extends Component {
     return (
       <div className="checklist">
         <ul>{tasks}</ul>
+        <input
+          type="text"
+          className="checklist--add-task"
+          placeholder="Key in a task and hit enter"
+        />
       </div>
     );
   }
