4f3ad3878a1e607b1ddf08e2a00008a8a9c413c8
devak23 <developer.ak23@gmail.com>
2018-12-02 02:51:17 +0530

    animated shopping list ready

--- animated-shopping-list/src/App.js
+++ animated-shopping-list/src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, name: "milk" },
+      { id: 2, name: "yogurt" },
+      { id: 3, name: "eggs" },
+      { id: 4, name: "cheese" },
+      { id: 5, name: "bread" }
+    ]
+  };
+
+  handleChange = e => {
+    if (e.key === "Enter") {
+      const newItem = { id: Date.now(), name: e.target.value };
+      const newItems = this.state.items.concat(newItem);
+      e.target.value = "";
+      this.setState({ items: newItems });
+    }
+  };
+
+  handleRemove = i => {
+    let newItems = this.state.items;
+    newItems.splice(i, 1);
+    this.setState({ items: newItems });
+  };
+
   render() {
+    const shoppingItems = this.state.items.map((item, i) => (
+      <div className="item" key={item.id} onClick={() => this.handleRemove(i)}>
+        {item.name}
+      </div>
+    ));
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={300}
+          transitionAppear={true}
+          transitionAppearTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          {shoppingItems}
+        </ReactCSSTransitionGroup>
+        <input
+          type="text"
+          value={this.state.newItem}
+          onKeyDown={this.handleChange}
+        />
       </div>
     );
   }
