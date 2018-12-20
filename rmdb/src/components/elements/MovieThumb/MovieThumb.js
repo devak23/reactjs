ba3632ab0300147276f28e61c64271545946dce9
devak23 <developer.ak23@gmail.com>
2018-12-20 19:48:24 +0530

    Added a title below each movie thumb

--- rmdb/src/components/elements/MovieThumb/MovieThumb.js
+++ rmdb/src/components/elements/MovieThumb/MovieThumb.js
@@ -5,6 +5,7 @@ const MovieThumb = props => {
   return (
     <div className="rmdb-moviethumb">
       <img src={props.image} alt="movie thumb" />
+      <div className="rmdb-moviethumb-title">{props.movieName}</div>
     </div>
   );
 };
