21a5167f300edc7beac340fc0dd7bcc5ff6d26db
devak23 <developer.ak23@gmail.com>
2019-01-02 10:45:57 +0530

    added some sample propTypes. Need to add them to the rest

--- rmdb/src/components/elements/FourColGrid/FourColGrid.js
+++ rmdb/src/components/elements/FourColGrid/FourColGrid.js
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./FourColGrid.css";
 
-const FourColGrid = props => {
+const FourColGrid = ({ children, loading, header }) => {
   const renderElements = () => {
     const gridElements =
-      props.children &&
-      props.children.map((element, i) => {
+      children &&
+      children.map((element, i) => {
         return (
           <div key={i} className="rmdb-grid-element">
             {element}
@@ -18,10 +19,15 @@ const FourColGrid = props => {
 
   return (
     <div className="rmdb-grid">
-      {props.header && !props.loading && <h1>{props.header}</h1>}
+      {header && !loading && <h1>{header}</h1>}
       <div className="rmdb-grid-content">{renderElements()}</div>
     </div>
   );
+};
+
+FourColGrid.propTypes = {
+  header: PropTypes.string,
+  loading: PropTypes.bool
 };
 
 export default FourColGrid;
--- rmdb/src/components/elements/MovieThumb/MovieThumb.js
+++ rmdb/src/components/elements/MovieThumb/MovieThumb.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./MovieThumb.css";
 
 const MovieThumb = props => {
@@ -7,15 +8,15 @@ const MovieThumb = props => {
   const { movieId, movieName } = props;
   return (
     <div className="rmdb-moviethumb">
-      {!props.clickable ? (
-        image
-      ) : (
-        <Link to={{ pathname: `/${movieId}`, movieName: `${movieName}` }}>
-          {image}
-        </Link>
-      )}
+      {!props.clickable ? image : <Link to={{ pathname: `/${movieId}`, movieName: `${movieName}` }}>{image}</Link>}
     </div>
   );
+};
+
+MovieThumb.propTypes = {
+  image: PropTypes.string,
+  movieId: PropTypes.number,
+  movieName: PropTypes.string
 };
 
 export default MovieThumb;
