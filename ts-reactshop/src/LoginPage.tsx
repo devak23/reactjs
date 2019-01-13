554c916808ef459621fc64a7b55b24397714d1ff
devak23 <developer.ak23@gmail.com>
2019-01-13 20:14:21 +0530

    added a redirect route

---
+++ ts-reactshop/src/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductsPage.css';
+
+const LoginPage: React.SFC = () => {
+  return (
+    <div className='container-page'>
+      <h2>You need to login</h2>
+    </div>
+  );
+};
+
+export default LoginPage;
--- ts-reactshop/src/Routes.tsx
+++ ts-reactshop/src/Routes.tsx
@@ -1,25 +1,42 @@
-import React, { SFC } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AdminPage from './AdminPage';
 import ProductsPage from './ProductsPage';
 import ProductDetailPage from './ProductDetailPage';
 import NotFoundPage from './NotFoundPage';
+import LoginPage from './LoginPage';
 import Header from './Header';
 
-const Routes: SFC = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/products' component={ProductsPage} />
-          <Route exact path='/products/:id' component={ProductDetailPage} />
-          <Route path='/admin' component={AdminPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
-};
+interface IState {
+  loggedIn: boolean;
+}
+
+class Routes extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      loggedIn: true
+    };
+  }
+
+  render() {
+    const loggedIn = this.state.loggedIn;
+
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path='/products' component={ProductsPage} />
+            <Route exact path='/products/:id' component={ProductDetailPage} />
+            <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
+            <Route exact path='/login' component={LoginPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default Routes;
