22442863b36f0c3872459957a55335580d627714
devak23 <developer.ak23@gmail.com>
2019-01-14 11:51:04 +0530

    added a Prompt Route when the user navigates away

--- ts-reactshop/src/ProductDetailPage.tsx
+++ ts-reactshop/src/ProductDetailPage.tsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { Prompt, RouteComponentProps } from 'react-router-dom';
 import { IProduct, products } from './ProductData';
 
 // key thing is we are going to use RouteComponentProps to access the id parameter in
@@ -32,20 +32,30 @@ class ProductDetailPage extends Component<Props, IState> {
     this.setState({ added: true });
   };
 
+  private navAwayMessage = () => 'Are you sure you want to leave without adding the product?';
+
+  private getPrice(product: IProduct) {
+    if (!product) {
+      console.log('product is null');
+      return null;
+    }
+    const price = new Intl.NumberFormat('en-US', {
+      currency: 'USD',
+      style: 'currency'
+    }).format(product.price);
+    return price;
+  }
+
   public render() {
     const product = this.state.product;
     return (
       <div className='page-container'>
+        <Prompt when={!this.state.added} message={this.navAwayMessage} />
         {product && (
           <Fragment>
             <h1>{product.name}</h1>
             <p>{product.description}</p>
-            <p className='product-price'>
-              {new Intl.NumberFormat('en-US', {
-                currency: 'USD',
-                style: 'currency'
-              }).format(product.price)}
-            </p>
+            <p className='product-price'>{this.getPrice(product)}</p>
             <button onClick={this.handleAddClick}>Add to Cart</button>
           </Fragment>
         )}
