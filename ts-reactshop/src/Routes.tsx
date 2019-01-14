b4ed3bd6d6820dda5c27248feda583d76108f4e6
devak23 <developer.ak23@gmail.com>
2019-01-14 23:50:57 +0530

    added animation

--- ts-reactshop/src/Routes.tsx
+++ ts-reactshop/src/Routes.tsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect, RouteComponentProps, Router } from 'react-router-dom';
 import AdminPage from './AdminPage';
 import ProductsPage from './ProductsPage';
 import ProductDetailPage from './ProductDetailPage';
 import NotFoundPage from './NotFoundPage';
 import LoginPage from './LoginPage';
 import Header from './Header';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './Routes.css';
 
 interface IState {
   loggedIn: boolean;
 }
 
-class Routes extends Component<{}, IState> {
-  constructor(props: {}) {
+class Routes extends Component<RouteComponentProps, IState> {
+  constructor(props: RouteComponentProps) {
     super(props);
     this.state = {
       loggedIn: true
@@ -23,23 +25,33 @@ class Routes extends Component<{}, IState> {
     const loggedIn = this.state.loggedIn;
 
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Switch>
-            <Route exact path='/products' component={ProductsPage} />
-            <Route exact path='/products/:id' component={ProductDetailPage} />
-            <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
-            <Route exact path='/login' component={LoginPage} />
-            <Route exact path=''>
-              <Redirect to='/products' />
-            </Route>
-            <Route component={NotFoundPage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Header />
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} timeout={500} classNames='animate'>
+            <Switch>
+              <Route exact path='/products' component={ProductsPage} />
+              <Route exact path='/products/:id' component={ProductDetailPage} />
+              <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
+              <Route exact path='/login' component={LoginPage} />
+              <Route exact path=''>
+                <Redirect to='/products' />
+              </Route>
+              <Route component={NotFoundPage} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
     );
   }
 }
 
-export default Routes;
+const RoutesWrap: React.SFC = () => {
+  return (
+    <BrowserRouter>
+      <Route component={Routes} />
+    </BrowserRouter>
+  );
+};
+
+export default RoutesWrap;
