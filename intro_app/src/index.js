54876f80cb8d4ec122b00c9ed83cba4ef8660821
devak23 <developer.ak23@gmail.com>
2019-01-17 21:13:58 +0530

    Multiple components rendering

--- intro_app/src/index.js
+++ intro_app/src/index.js
@@ -4,8 +4,11 @@ let ReactDOM = require('react-dom');
 let myComponent = <h1>Hello from MyComponent</h1>;
 console.log(myComponent);
 
-let Greeting = function() {
-  return <h1>Hello from My Component</h1>;
+let Greeting = function(props) {
+  let style = {
+    color: props.color
+  };
+  return <h1 style={style}>{props.message}</h1>;
 };
 
 /* this will be rendered on the screen even when myComponent doesn't start with upper case 'M'. this is
@@ -21,4 +24,22 @@ let Greeting = function() {
    will render the element on the screen. Therefore the above Greeting functional component has to be either wrapped in
    React.createElement(Greeting) or it has to be passed to render method using JSX like so 
  */
-ReactDOM.render(<Greeting />, document.getElementById('root'));
+// ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+// The parameter to the Greeting component gets wrapped into an object called props which gets passed to our function above.
+ReactDOM.render(<Greeting message='Hello!' />, document.getElementById('root1'));
+// We can have multiple DIV's in our index.html and can render separate messages to all of them.
+ReactDOM.render(<Greeting message='Namaste!' color='orange' />, document.getElementById('root2'));
+ReactDOM.render(<Greeting message='Konnichiva!' color='pink' />, document.getElementById('root3'));
+ReactDOM.render(<Greeting message='Bounjour!' color='purple' />, document.getElementById('root4'));
+
+// If there are multiple elements to be rendered in a single html div, we need to wrap it under a single div or use
+// React.Fragment
+ReactDOM.render(
+  <React.Fragment>
+    <Greeting message='Hallo' color='brown' />
+    <Greeting message='Ciao' color='green' />
+    <Greeting message='Привет' color='red' />
+  </React.Fragment>,
+  document.getElementById('root5')
+);
