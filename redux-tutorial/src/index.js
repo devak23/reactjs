ba01c081591eed027b0a3f7b7e5a518a95f4981c
devak23 <developer.ak23@gmail.com>
2019-01-23 01:35:35 +0530

    Working with thunk ... WIP

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from 'redux';
 import allReducers from './reducers';
 // a provider provides access to the app to connect to the store
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+
+const allStoreEnhancers = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const store = createStore(
   allReducers,
@@ -13,7 +19,7 @@ const store = createStore(
     products: [{ name: 'iPhone' }],
     user: 'Michael'
   },
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  allStoreEnhancers
 );
 
 console.log('Store created with initial state: ', store.getState());
