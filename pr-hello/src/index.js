d892c93d6b4afd2d7e6add8bbc9dbc3540c0aac4
devak23 <developer.ak23@gmail.com>
2019-11-03 01:27:40 +0530

    modifications

--- pr-hello/src/index.js
+++ pr-hello/src/index.js
@@ -1,37 +1,45 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 function Hello() {
-    return (
-        <div>
-            <h2>Hello World! from PureReact</h2>
-            <p>
-                So this text comes from a function called Hello
-                <pre><code>function Hello() </code></pre>
-            </p>
-            <p>Initially, I coded the function with lower case hello</p>
-            <pre><code>function hello() </code></pre>
-            <p>
-                So obviously the tag in the render() method has to be called
-                &lt;hello&gt;. To my surprise when i started the server, react didn't
-                render anything! The reason is -  react tries to render lower cased function
-                as html element and uppper cased functions are treated by React as
-                React Components.
-            </p>
-            <p>
-                Since there is no html element called &lt;hello /&gt;, react didn't
-                render anything.
-                Changing the function name to Hello and changing the tag to &lt;Hello/&gt;
-                fixed the problem and this text is now visible.
-            </p>
-            <p>
-                This is a small thing Dave missed to mention, but then he expected me to
-                code "exactly" the way he had mentioned in the book, and I didn't! so...we are good! :)
-            </p>
-            <p>Good learning!</p>
-        </div>
-    )
+  return (
+    <div className='container'>
+      <div className='mtop2'>
+        <h2>Hello World! from PureReact</h2>
+        <hr className='simple' />
+        <p>
+          So this text comes from a function called Hello
+          <pre>
+            <code>function Hello() </code>
+          </pre>
+        </p>
+        <p>Initially, I coded the function with lower case hello</p>
+        <pre>
+          <code>function hello() </code>
+        </pre>
+        <p>
+          So obviously the tag in the render() method has to be called &lt;hello&gt;. To my surprise, react didn't
+          render anything at the server start. The reason is - react tries to render lower cased functions as html
+          element and uppper cased functions as React Components. A better answer can be found{' '}
+          <a href='https://stackoverflow.com/questions/30373343/reactjs-component-names-must-begin-with-capital-letters'>
+            here
+          </a>
+        </p>
+        <p>
+          Since there is no html element called &lt;hello /&gt;, react didn't render anything. Changing the function
+          name to Hello and changing the tag to &lt;Hello/&gt; fixed the problem and this text is now visible.
+        </p>
+        <p>
+          Small thing Dave missed to mention, but he expected me to code "exactly" the way he had mentioned in the book,
+          and I didn't! so...we are good! &#128519;
+        </p>
+        <p>Good learning!</p>
+        <hr className='simple' />
+      </div>
+    </div>
+  );
 }
 
-render(<Hello />, document.querySelector("#root"))
+render(<Hello />, document.querySelector('#root'));
