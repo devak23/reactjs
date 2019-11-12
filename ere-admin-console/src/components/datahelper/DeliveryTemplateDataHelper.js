2d0e7feabe6b5911a8124d3f75bec7c17ceb8e4d
devak23 <developer.ak23@gmail.com>
2019-11-12 18:27:29 +0530

    added ALL to countries list

--- ere-admin-console/src/components/datahelper/DeliveryTemplateDataHelper.js
+++ ere-admin-console/src/components/datahelper/DeliveryTemplateDataHelper.js
@@ -1,6 +1,6 @@
 export default {
   getCountries: function() {
-    return Object.assign({}, ["AUSTRALIA","CHINA","GERMANY","HONG KONG" ,"INDIA","INDONESIA","IRELAND","JAPAN","KOREA","LUXEMBOURG","MALAYSIA","MALAYSIA (LABUAN)","MEXICO","NEW ZEALAND" ,"PHILIPPINES","SINGAPORE","TAIWAN","THAILAND","UNITED KINGDOM" ,"US","VIETNAM"]);
+    return Object.assign({}, ["ALL", "AUSTRALIA","CHINA","GERMANY","HONG KONG" ,"INDIA","INDONESIA","IRELAND","JAPAN","KOREA","LUXEMBOURG","MALAYSIA","MALAYSIA (LABUAN)","MEXICO","NEW ZEALAND" ,"PHILIPPINES","SINGAPORE","TAIWAN","THAILAND","UNITED KINGDOM" ,"US","VIETNAM"]);
   },
   getStatuses: function() {
     return Object.assign({}, ['','Pending','Approved', 'Rejected', 'Partial']);
