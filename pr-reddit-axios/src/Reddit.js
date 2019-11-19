74bc7285db17b0e80563425fdd27e81c6db19e69
devak23 <developer.ak23@gmail.com>
2019-11-20 01:24:16 +0530

    added an axios example

---
+++ pr-reddit-axios/src/Reddit.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const isEmpty = (obj) => {
+  if (obj === null) return true;
+  if (obj.length === 0) return true;
+  if (typeof (obj) !== 'object') return true;
+  if (Object.keys(obj).length === 0) return true;
+  return false;
+}
+
+const Reddit = ({ subreddit }) => {
+  const [posts, setPosts] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(response => {
+        const posts = response.data.data.children.map(obj => obj.data);
+        setPosts(posts);
+      })
+  });
+
+  console.log(posts);
+  return (
+    <div>
+      <h3>{`/r/${subreddit}`}</h3>
+      <ul>
+        {!isEmpty(posts) && posts.map(post =>
+          <li key={post.id}>
+            <a href={`https://www.reddit.com${post.permalink}`}>{post.title}</a>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default Reddit;
---
+++ pr-reddit-axios/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Reddit from './Reddit';
+
+ReactDOM.render(<Reddit subreddit='reactjs' />, document.querySelector('#root'));
