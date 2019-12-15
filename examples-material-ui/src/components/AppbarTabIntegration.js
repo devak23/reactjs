c08cb15e7e1be88efe539d1f7811ab9b240ecdf7
devak23 <developer.ak23@gmail.com>
2019-12-15 18:53:16 +0530

    added Tabs with AppBar integration

--- examples-material-ui/src/components/AppbarTabIntegration.js
+++ examples-material-ui/src/components/AppbarTabIntegration.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import compose from 'recompose/compose';
+import withWidth from '@material-ui/core/withWidth';
 import { withStyles } from '@material-ui/core/styles';
 import { AppBar, Tabs, Tab, Typography } from '@material-ui/core';
 
@@ -8,32 +10,36 @@ const styles = (theme) => ({
 		backgroundColor: theme.palette.background.paper
 	},
 	tabContent: {
-		padding: theme.spacing.unit * 2
+		padding: theme.spacing(2)
 	}
 });
 
-const AppbarTabIntegration = ({ classes }) => {
+const AppbarTabIntegration = ({ classes, width }) => {
 	const [ value, setValue ] = useState(0);
 
 	const handleOnChange = (e, value) => setValue(value);
 	return (
 		<div className={classes.root}>
 			<AppBar position='static'>
-				<Tabs value={value} onChange={handleOnChange}>
+				<Tabs
+					value={value}
+					onChange={handleOnChange}
+					centered
+					variant={[ 'xs', 'sm' ].includes(width) ? null : 'fullWidth'}
+				>
 					<Tab label='Basic Information' />
 					<Tab label='Accounts' />
 					<Tab label='Data and Display' />
 					<Tab label='Schedule' />
-					<Tab label='Sharing' />
 				</Tabs>
 			</AppBar>
 			{value === 0 && <Typography className={classes.tabContent}>Basic Information</Typography>}
 			{value === 1 && <Typography className={classes.tabContent}>Accounts</Typography>}
 			{value === 2 && <Typography className={classes.tabContent}>Data and display</Typography>}
 			{value === 3 && <Typography className={classes.tabContent}>Schedule</Typography>}
-			{value === 4 && <Typography className={classes.tabContent}>Sharing</Typography>}
 		</div>
 	);
 };
 
-export default withStyles(styles)(AppbarTabIntegration);
+export default compose(withWidth(), withStyles(styles))(AppbarTabIntegration);
+//export default withWidth(withStyles(styles))(AppbarTabIntegration);
