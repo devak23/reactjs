71a12aa8f0035c80233be54205c94071545860b0
devak23 <developer.ak23@gmail.com>
2019-12-25 12:17:09 +0530

    completed positioning snackbar

--- examples-material-ui/src/components/SnackbarPositions.js
+++ examples-material-ui/src/components/SnackbarPositions.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './widgets/Header';
 import { withStyles } from '@material-ui/styles';
+import { FormControl, FormLabel, RadioGroup, Radio, FormControlLabel, Snackbar } from '@material-ui/core';
 
 const styles = (theme) => ({
 	root: {
@@ -8,14 +9,51 @@ const styles = (theme) => ({
 	},
 	content: {
 		width: 350
+	},
+	formControl: {
+		margin: theme.spacing(3)
 	}
 });
 
 const SnackbarPositions = ({ classes }) => {
+	const [ vertical, setVertical ] = useState('bottom');
+	const [ horizontal, setHorizontal ] = useState('left');
+
+	const handleVerticalChange = (e) => setVertical(e.target.value);
+
+	const handleHorizontalChange = (e) => setHorizontal(e.target.value);
+
 	return (
 		<div className={classes.root}>
 			<Header title='Snackbar Positions' />
-			<div className={classes.content} />
+			<div className={classes.content}>
+				<FormControl component='fieldset' className={classes.formControl}>
+					<FormLabel component='legend'>Vertical</FormLabel>
+					<RadioGroup
+						name='vertical'
+						className={classes.group}
+						value={vertical}
+						onChange={handleVerticalChange}
+					>
+						<FormControlLabel value='top' control={<Radio />} label='top' />
+						<FormControlLabel value='bottom' control={<Radio />} label='bottom' />
+					</RadioGroup>
+				</FormControl>
+				<FormControl component='fieldset' className={classes.formControl}>
+					<FormLabel component='legend'>Horizontal</FormLabel>
+					<RadioGroup
+						name='horizontal'
+						className={classes.group}
+						value={horizontal}
+						onChange={handleHorizontalChange}
+					>
+						<FormControlLabel value='left' control={<Radio />} label='Left' />
+						<FormControlLabel value='center' control={<Radio />} label='Center' />
+						<FormControlLabel value='Right' control={<Radio />} label='Right' />
+					</RadioGroup>
+				</FormControl>
+				<Snackbar anchorOrigin={{ vertical, horizontal }} open={true} message='Positioned snackbar' />
+			</div>
 		</div>
 	);
 };
