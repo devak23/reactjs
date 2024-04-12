a73e1a0803df7ab0b99cd9bc4cf2bc66ce62413d
Abhay Kulkarni <developer.ak23@gmail.com>
2024-04-13 03:53:40 +0530

    Changed layout is now displayed

--- playing-with-gridlayout/src/components/DesignArena.js
+++ playing-with-gridlayout/src/components/DesignArena.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import GridLayout from "react-grid-layout";
 import "./DesignArena.css"
 import {getCards, getLayout} from "../data/Elements";
 
-class DesignArena extends React.Component {
-  render() {
+const DesignArena = () => {
+    const [layout, setLayout] = useState(getLayout())
+
+    const handleShowLayout = () => {
+        console.log(layout);
+    }
+
+    const handleLayoutChange = (newLayout) => {
+        setLayout(newLayout);
+    }
+
     return (
-      <div className="layout">
-        <GridLayout
-            layout={getLayout()}
-            cols={12}
-            rowHeight={30}
-            width={1200}
-            verticalCompact={false}
-        >
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <div className="grid">
+                <GridLayout
+                    layout={layout}
+                    cols={12}
+                    rowHeight={30}
+                    width={1200}
+                    compactType={null}
+                    margin={[5, 5]}
+                    containerPadding={[1, 1]}
+                    onLayoutChange={handleLayoutChange}
+                >
+                    {getCards()}
 
-          { getCards() }
-
-        </GridLayout>
-      </div>
+                </GridLayout>
+            </div>
+            <button style={{margin: "auto", height: 30}} onClick={handleShowLayout}>Show Layout</button>
+        </div>
     )
-  }
 }
 
 export default DesignArena;
--- playing-with-gridlayout/src/data/Elements.js
+++ playing-with-gridlayout/src/data/Elements.js
@@ -4,27 +4,20 @@ const elements = [
     'Transaction Type', 'Senders Reference', 'Sender Bank', 'Ordering Bank', 'Ordering Customer', 'Logo'
 ];
 
-const Card = (name, index) => <div className="card" key={index}>{name}</div>
-
-const cards = [
-    elements.map((e, i) =>  <Card name={e} index={i} />)
-]
-
-
 export const getLayout = () => {
-    return [
-        cards.map((e, i) => {
-            return {
-                i: i,
-                x: i - 1,
-                y: 0,
-                w: 1,
-                h: 1
-            }
-        })
-    ];
+    return elements.map((e, i) => {
+        return {
+            i: i,
+            x: i,
+            y: 0,
+            w: 1,
+            h: 1
+        }
+    });
 }
+
 
 export const getCards = () => {
-    return cards;
+    return elements.map((name, index) => <div className="card" key={index}>{name}</div>);
 }
+
